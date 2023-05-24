75b312918a3fd5825ee796ad947f6a8da9605f1f
mly <1445531395@qq.com>
2023-05-24 12:25:36 +0800

    add:项目初始化

---
+++ src/main/java/demo/Demo1.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * @author mly
+ * @date 2023/5/24 12:24
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
