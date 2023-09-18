4f9cfc8eeabe23aecd3d69fde72b3facc76c61ca
LemonYu997 <1445531395@qq.com>
2023-09-18 22:11:18 +0800

    add:T136

---
+++ src/main/java/demo/c0easy/T136.java
@@ -0,0 +1,29 @@
+package demo.c0easy;
+
+/**
+ * T136 只出现一次的数字 https://leetcode.cn/problems/single-number/description/
+ * 给定一个非空整数数组nums，除了某个元素只出现一次以外，其余每个元素均出现两次，找出只出现一次的数字
+ *
+ * 示例：
+ * 输入1：nums = [2,2,1]
+ * 输出1：1
+ *
+ * 输入2：nums = [4,1,2,1,2]
+ * 输出2：4
+ *
+ * 输入3：nums = [1]
+ * 输出3:1
+ */
+public class T136 {
+    /**
+     * 官方解法：位运算 异或运算
+     */
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+
+        return single;
+    }
+}
