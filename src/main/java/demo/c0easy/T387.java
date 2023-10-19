4f955188b7e39a6712bee0e54e340fb88a82b4cd
LemonYu997 <1445531395@qq.com>
2023-10-20 00:14:54 +0800

    T387

--- src/main/java/demo/c0easy/T387.java
+++ src/main/java/demo/c0easy/T387.java
@@ -1,5 +1,8 @@
 package demo.c0easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 387 字符串中的第一个唯一字符 https://leetcode.cn/problems/first-unique-character-in-a-string/
  * 给定一个字符串s，找到它的第一个不重复的字符，并返回它的索引
@@ -17,7 +20,7 @@ package demo.c0easy;
  */
 public class T387 {
     /**
-     * 自己实现：双指针
+     * 自己实现：双指针暴力遍历
      */
     public int firstUniqChar(String s) {
         char[] chars = s.toCharArray();
@@ -36,6 +39,26 @@ public class T387 {
             }
 
             if (flag) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 官方解法1：使用哈希表存储次数
+     */
+    public int firstUniqChar1(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+
+        for (char c : chars) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            if (map.get(chars[i]) == 1) {
                 return i;
             }
         }
