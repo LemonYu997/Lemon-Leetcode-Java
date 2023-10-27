3bddecf4b43c3cbe74be59f64296acf913b3d574
LemonYu997 <1445531395@qq.com>
2023-10-27 22:54:05 +0800

    T459

--- src/main/java/demo/c0easy/T459.java
+++ src/main/java/demo/c0easy/T459.java
@@ -67,4 +67,30 @@ public class T459 {
 
         return false;
     }
+
+    /**
+     * 官方解法1：枚举
+     * 大体思路同上
+     */
+    public boolean repeatedSubstringPattern3(String s) {
+        int n = s.length();
+
+        for (int i = 1; i * 2 <= n; i++) {
+            if (n % i == 0) {
+                boolean match = true;
+                for (int j = i; j < n; j++) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
