5a08f922f418cfc8b9c7ca4bb1a81af7d288b692
lemonyu <1445531395@qq.com>
2024-03-07 16:14:47 +0800

    add：1046

---
+++ src/main/java/problems/c0easy/T1046.java
@@ -0,0 +1,38 @@
+package problems.c0easy;
+
+import java.util.PriorityQueue;
+
+/**
+ * 1046 最后一块石头的重量 https://leetcode.cn/problems/last-stone-weight/description/
+ * 有一堆石头，每块石头的重量都是正整数。
+ * 每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。
+ * 假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+ * 如果 x == y，那么两块石头都会被完全粉碎；
+ * 如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+ * 最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。
+ *
+ * 示例：
+ * 输入1：[2,7,4,1,8,1]
+ * 输出1：1
+ */
+public class T1046 {
+    /**
+     * 官方解法：将所有石头放入最大堆中，每次取出两个最大的石头
+     * 使用优先队列作为最大堆
+     */
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int stone : stones) {
+            pq.offer(stone);
+        }
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            if (a > b) {
+                pq.offer(a - b);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
