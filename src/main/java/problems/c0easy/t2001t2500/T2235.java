f04b65598139371f3ad10a707b6f923795cec24e
LemonYu997 <1445531395@qq.com>
2025-03-20 21:30:37 +0800

    add:2235

---
+++ src/main/java/problems/c0easy/t2001t2500/T2235.java
@@ -0,0 +1,21 @@
+package problems.c0easy.t2001t2500;
+
+/**
+ * 2235 两整数相加 https://leetcode.cn/problems/add-two-integers/description/
+ * 给你两个整数 num1 和 num2，返回这两个整数的和。
+ *
+ * 示例：
+ * 输入1：num1 = 12, num2 = 5
+ * 输出1：17
+ *
+ * 输入2：num1 = -10, num2 = 4
+ * 输出2：-6
+ */
+public class T2235 {
+    /**
+     * 自己实现
+     */
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
