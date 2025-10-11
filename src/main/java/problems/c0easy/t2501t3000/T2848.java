6962fd9fdc08c28a25285103bca23862c804ac1e
LemonYu <1445531395@qq.com>
2025-10-11 09:25:58 +0800

    add:2848

---
+++ src/main/java/problems/c0easy/t2501t3000/T2848.java
@@ -0,0 +1,40 @@
+package problems.c0easy.t2501t3000;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 2848 与车相交的点 https://leetcode.cn/problems/points-that-intersect-with-cars/description/
+ * 给你一个下标从 0 开始的二维整数数组 nums 表示汽车停放在数轴上的坐标。
+ * 对于任意下标 i，nums[i] = [starti, endi] ，
+ * 其中 starti 是第 i 辆车的起点，endi 是第 i 辆车的终点。
+ * 返回数轴上被车 任意部分 覆盖的整数点的数目。
+ *
+ * 示例：
+ * 输入1：nums = [[3,6],[1,5],[4,7]]
+ * 输出1：7
+ *
+ * 输入2：nums = [[1,3],[5,8]]
+ * 输出2：7
+ *
+ * 提示：
+ * 1 <= nums.length <= 100
+ * nums[i].length == 2
+ * 1 <= starti <= endi <= 100
+ */
+public class T2848 {
+    /**
+     * 自己实现：因为 1 <= starti <= endi <= 100，统计 100以内的数字出现次数即可
+     */
+    public int numberOfPoints(List<List<Integer>> nums) {
+        int[] temp = new int[101];
+
+        for (List<Integer> list : nums) {
+            for (int i = list.get(0); i <= list.get(1); i++) {
+                temp[i] = 1;
+            }
+        }
+
+        return Arrays.stream(temp).sum();
+    }
+}
