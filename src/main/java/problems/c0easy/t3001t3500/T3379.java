eb7e1bb117275ed8e372b92879042a3ff9ffaddb
LemonYu <1445531395@qq.com>
2026-03-26 14:29:09 +0800

    add:3379

---
+++ src/main/java/problems/c0easy/t3001t3500/T3379.java
@@ -0,0 +1,32 @@
+package problems.c0easy.t3001t3500;
+
+/**
+ * 3379 转换数组 https://leetcode.cn/problems/transformed-array/description/
+ * 给你一个整数数组 nums，它表示一个循环数组。请你遵循以下规则创建一个大小 相同 的新数组 result ：
+ * 对于每个下标 i（其中 0 <= i < nums.length），独立执行以下操作：
+ * 如果 nums[i] > 0：从下标 i 开始，向 右 移动 nums[i] 步，在循环数组中落脚的下标对应的值赋给 result[i]。
+ * 如果 nums[i] < 0：从下标 i 开始，向 左 移动 abs(nums[i]) 步，在循环数组中落脚的下标对应的值赋给 result[i]。
+ * 如果 nums[i] == 0：将 nums[i] 的值赋给 result[i]。
+ * 返回新数组 result。
+ * 注意：由于 nums 是循环数组，向右移动超过最后一个元素时将回到开头，向左移动超过第一个元素时将回到末尾。
+ *
+ * 示例：
+ * 输入1：nums = [3,-2,1,1]
+ * 输出1：[1,1,1,3]
+ *
+ * 输入2：nums = [-1,4,-1]
+ * 输出2：[-1,-1,4]
+ */
+public class T3379 {
+    /**
+     * 官方解法：模拟遍历
+     */
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = nums[((i + nums[i]) % n + n) % n];
+        }
+        return res;
+    }
+}
