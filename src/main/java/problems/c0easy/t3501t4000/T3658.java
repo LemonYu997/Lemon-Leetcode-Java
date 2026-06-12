266968745f10eb1446bad65238ef654fcfd15f15
LemonYu <1445531395@qq.com>
2026-06-12 14:40:56 +0800

    add:3658

---
+++ src/main/java/problems/c0easy/t3501t4000/T3658.java
@@ -0,0 +1,24 @@
+package problems.c0easy.t3501t4000;
+
+/**
+ * 3658 奇数和与偶数和的最大公约数 https://leetcode.cn/problems/gcd-of-odd-and-even-sums/description/
+ * 给你一个整数 n。请你计算以下两个值的 最大公约数（GCD）：
+ * sumOdd：最小的 n 个正奇数的总和。
+ * sumEven：最小的 n 个正偶数的总和。
+ * 返回 sumOdd 和 sumEven 的 GCD。
+ *
+ * 示例：
+ * 输入1：n = 4
+ * 输出1：4
+ *
+ * 输入2：n = 5
+ * 输出2：5
+ */
+public class T3658 {
+    /**
+     * 社区解法：根据数学运算，结果就是n
+     */
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
